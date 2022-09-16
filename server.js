5a95559fc422642d9ff5376052d32e2413fed1a8
night <yseddiq@gmail.com>
2022-09-16 17:22:44 -0400

    added html routes, and got them to work

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.static('public'));
 const path = require('path');
 //const app = express();
 
+//htmlRoutes
+app.get('/notes', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+//api routes
 app.get('/api/notes', (req,res)=>{
     res.header("Content-Type", 'application/json')
     res.sendFile(path.join(__dirname,'./db/db.json'));
