0ffd285f4860af203676a411ae27a83a9505e5e6
night <yseddiq@gmail.com>
2022-09-16 16:47:45 -0400

    got /api/notes to basically work

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,5 +2,5 @@ const path = require('path');
 const app = express();
 
 app.get('/api/notes', (req,res)=>{
-
+    res.send('this does work');
 })
--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
+const path = require('path');
+//const app = express();
+
+app.get('/api/notes', (req,res)=>{
+    res.send('this does work');
+})
+
 app.listen(PORT, ()=>{
     console.log('server is up and running')
 })
