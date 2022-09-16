e3d5abd2a4eecf9bbf9236250b72762f413409af
night <yseddiq@gmail.com>
2022-09-16 19:11:08 -0400

    got apiRoutes to work after refactor

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -5,14 +5,14 @@ const {v4 : uuidv4} = require('uuid')
 
 const id = uuidv4();
 
-app.get('/api/notes', (req,res)=>{
+router.get('/api/notes', (req,res)=>{
     res.header("Content-Type", 'application/json')
     res.sendFile(path.join(__dirname,'../db/db.json'));
 });
 
 
 //following code posts new data into the db.json and hopefully adds a unique ID.
-app.post('/api/notes',(req,res)=>{
+router.post('/api/notes',(req,res)=>{
     const noteTitle = req.body.title;
     const noteText = req.body.text;
     
