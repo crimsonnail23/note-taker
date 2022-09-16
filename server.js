736e63f2eb50aeee098ceb9b56b182cbd2193f14
night <yseddiq@gmail.com>
2022-09-16 17:41:57 -0400

    commiting to get help from askBCS

--- server.js
+++ server.js
@@ -20,9 +20,16 @@ app.get('*', (req,res)=>{
 })
 
 //api routes
+//following code retrives data from db.json
+const {v4 : uuidv4} = require('uuid')
+const id = uuidv4();
 app.get('/api/notes', (req,res)=>{
     res.header("Content-Type", 'application/json')
     res.sendFile(path.join(__dirname,'./db/db.json'));
+})
+//following code posts new data into the db.json and hopefully adds a unique ID.
+app.post('/api/notes',(req,res)=>{
+    
 })
 
 app.listen(PORT, ()=>{
