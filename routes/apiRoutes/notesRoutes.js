b2983185f6a239c8a9c18d8dbe39bdc510fc5cc2
night <yseddiq@gmail.com>
2022-09-16 23:42:41 -0400

    got post route to work, and also added a unique id.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const router = require("express").Router();
 const {v4 : uuidv4} = require('uuid')
-
 const id = uuidv4();
 
 router.get('/notes', (req,res)=>{
@@ -15,11 +14,13 @@ router.post('/notes',(req,res)=>{
     const noteTitle = req.body.title;
     const noteText = req.body.text;
     
-    console.log(noteTitle, noteText)
+    req.body.id = id
+
+    console.log(noteTitle, noteText, req.body.id)
 
     //use document.querySelecter().value and red.body together to create the new data???
     
-    //res.send(noteTitle, noteText);
+    res.json({noteTitle, noteText});
 });
 
 module.exports = router;
